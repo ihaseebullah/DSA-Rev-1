fd88126d2b4ddd27ba38c346e9a6f1294d7f6bc5
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-12-18 17:55:41 +0500

    Ref: Added sub methods as well

---
+++ DLL.js
@@ -0,0 +1,66 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DLL {
+    constructor(value) {
+        let newNode = new Node(value)
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1
+    }
+
+    display() {
+        let iterator = this.head;
+        while (iterator) {
+            console.log(iterator.value)
+            iterator = iterator.next
+        }
+    }
+    dannasha(value) {
+        let pacha = new Node(value);
+        let iterator = this.head;
+        while (iterator.next) {
+            iterator = iterator.next;
+        }
+        iterator.next = pacha;
+        pacha.prev = iterator;
+        this.tail = pacha;
+        this.length++;
+    }
+
+    rewuse() {
+        let iterator = this.tail;
+        while (iterator) {
+            console.log(iterator.value)
+            iterator = iterator.prev;
+        }
+    }
+    insert(value, index) {
+        let newNode = new Node(value);
+        let iterator = this.head;
+        for (let i = 0; i < (index - 1); i++) {
+            iterator = iterator.next;
+        }
+        newNode.prev = iterator;
+        newNode.next = iterator.next;
+        iterator.next = newNode;
+        this.length++;
+    }
+}
+
+
+const myDoublyLinkedList = new DLL(1)
+
+myDoublyLinkedList.dannasha(2)
+myDoublyLinkedList.dannasha(3)
+myDoublyLinkedList.dannasha(4)
+
+// myDoublyLinkedList.rewuse()
+myDoublyLinkedList.display()
+myDoublyLinkedList.insert(9, 2)
+myDoublyLinkedList.display()
