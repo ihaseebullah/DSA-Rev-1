eb2abb3b05da1a24ba2aac378a9e7c2cf365faf5
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-12-18 17:03:13 +0500

    Ref: DLL

--- LinkedListPractice.js
+++ LinkedListPractice.js
@@ -13,3 +13,13 @@ reverse(){
     this.head.next=null;
     this.head=left;
 }
+//Middle Guy
+
+manzanai(){
+    const l=(this.length)/2;
+    let iterator=this.head;
+    for(let i=0;i<(l-1);i++){
+        iterator=iterator.next
+    }
+    return iterator.value
+}
