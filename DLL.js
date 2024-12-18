23faa510d59ad229b2930d2a05b441138e9f79ca
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-12-18 18:28:05 +0500

    Leetcode: Palindrome Checker

--- DLL.js
+++ DLL.js
@@ -33,19 +33,22 @@ class DLL {
         this.length++;
     }
 
-    swap(){
-       let head=this.head.value;
-       let tail=this.tail.value;
-       this.head.value=tail;
-       this.tail.value=head;
+    swap() {
+        let head = this.head.value;
+        let tail = this.tail.value;
+        this.head.value = tail;
+        this.tail.value = head;
     }
 
     rewuse() {
         let iterator = this.tail;
+        let totall = ""
         while (iterator) {
-            console.log(iterator.value)
+            // console.log(iterator.value)
+            totall = totall.concat(iterator.value)
             iterator = iterator.prev;
         }
+        return totall;
     }
     insert(value, index) {
         let newNode = new Node(value);
@@ -58,18 +61,45 @@ class DLL {
         iterator.next = newNode;
         this.length++;
     }
+    sada() {
+        let totall = ""
+        let iterator = this.head;
+        while (iterator) {
+            totall = totall.concat(`${iterator.value}`)
+            // console.log(`${iterator.value}`)
+            iterator = iterator.next;
+        }
+        return totall;
+    }
+
+    // palindrome(){
+
+    // }
 }
 
 
-const myDoublyLinkedList = new DLL(1)
+const myDoublyLinkedList = new DLL("H")
 
-myDoublyLinkedList.dannasha(2)
-myDoublyLinkedList.dannasha(3)
-myDoublyLinkedList.dannasha(4)
+myDoublyLinkedList.dannasha("A")
+myDoublyLinkedList.dannasha("T")
+// myDoublyLinkedList.dannasha(4)
 
 // myDoublyLinkedList.rewuse()
 // myDoublyLinkedList.display()
-myDoublyLinkedList.insert(9, 2)
-myDoublyLinkedList.insert(9, 4)
-myDoublyLinkedList.swap()
-myDoublyLinkedList.display()
+// myDoublyLinkedList.insert(9, 2)
+// myDoublyLinkedList.insert(9, 4)
+// myDoublyLinkedList.swap()
+// myDoublyLinkedList.display()
+
+function palindromeChecker() {
+    // console.log(myDoublyLinkedList.rewuse())
+    // console.log(myDoublyLinkedList.sada())
+    if (myDoublyLinkedList.rewuse() == myDoublyLinkedList.sada()) {
+        console.log("Perfect palindrome")
+        return true;
+    }
+    console.log("Non palindrome")
+    return false;
+}
+
+palindromeChecker()
