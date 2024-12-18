ea1f17ef93885ee233ecae0923a55d3b86cc3cd5
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-12-18 23:25:48 +0500

    Stack: Completed Stack Datastrucuter

--- Stack.js
+++ Stack.js
@@ -8,14 +8,38 @@ class Node {
 class Stack {
     constructor(value) {
         let firstNode = new Node(value);
-        this.first = firstNode;
+        this.top = firstNode;
+        firstNode.next = null;
     }
 
     shift(value) {
         let newNode = new Node(value);
+        newNode.next = this.top;
         this.top = newNode;
+    }
+
+    view() {
+        let iterator = this.top;
+        while (iterator) {
+            console.log(iterator.value)
+            iterator = iterator.next
+        }
+    }
+
+    unshift() {
+        this.top = this.top.next;
     }
 }
 
 
 let myStack = new Stack(1);
+myStack.shift(2);
+myStack.shift(3);
+myStack.shift(4);
+myStack.shift(5);
+myStack.shift(6);
+
+myStack.unshift()
+myStack.unshift()
+myStack.unshift()
+myStack.view()
